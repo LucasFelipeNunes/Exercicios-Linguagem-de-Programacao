7ace46309e088b32ed32fc823622e0140a908305
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-24 13:45:52 -0300

    Exercicios 11 a 14 Lista 1

--- Lista de Exercicios 1/Lista1Exercicios10a20.java
+++ Lista de Exercicios 1/Lista1Exercicios10a20.java
@@ -8,47 +8,54 @@ public class Lista1Exercicios10a20 {
 		do{
 			System.out.print("Digite o numero inteiro entre 10 e 20 do exercicio que voce gostaria de ver: ");
 			exercicio = ler.nextLine();
-			System.out.print(exercicio);
-			if(exercicio != "10" && exercicio != "11" && exercicio != "12" && exercicio != "13" && exercicio != "14" && exercicio != "15" && exercicio != "16" && exercicio != "17" && exercicio != "18" && exercicio != "19" && exercicio != "20"){
+			if(exercicio.intern() != "10" && exercicio.intern() != "11" && exercicio.intern() != "12" && exercicio.intern() != "13" && exercicio.intern() != "14" && exercicio.intern() != "15" && exercicio.intern() != "16" && exercicio.intern() != "17" && exercicio.intern() != "18" && exercicio.intern() != "19" && exercicio.intern() != "20"){
 				System.out.print("Valor invalido. ");
 			}
-		}while(exercicio != "10" && exercicio != "11" && exercicio != "12" && exercicio != "13" && exercicio != "14" && exercicio != "15" && exercicio != "16" && exercicio != "17" && exercicio != "18" && exercicio != "19" && exercicio != "20");
+		}while(exercicio.intern() != "10" && exercicio.intern() != "11" && exercicio.intern() != "12" && exercicio.intern() != "13" && exercicio.intern() != "14" && exercicio.intern() != "15" && exercicio.intern() != "16" && exercicio.intern() != "17" && exercicio.intern() != "18" && exercicio.intern() != "19" && exercicio.intern() != "20");
 		switch(exercicio){
 			case "10":
-				System.out.print("Enunciado: 10 - Qual alternativa possui apenas nomes válidos para arquivos de código fonte Java?\na. Fatec.java e Treinamentos.java.\nb. fatec.java e Treinamentos.Java.\nc. Fatec.JAVA e Treinamentos.java.\nd. FATEC.JAVA e Treinamentos.JAVA.\ne. Fatec.Java e Treinamentos.Java.");
+				System.out.print("\nEnunciado: 10 - Qual alternativa possui apenas nomes válidos para arquivos de código fonte Java?\na. Fatec.java e Treinamentos.java.\nb. fatec.java e Treinamentos.Java.\nc. Fatec.JAVA e Treinamentos.java.\nd. FATEC.JAVA e Treinamentos.JAVA.\ne. Fatec.Java e Treinamentos.Java.");
 				System.out.print("Resposta: a. Fatec.java e Treinamentos.java."); 
 			break;
 			case "11":
-			
+				System.out.print("\nEnunciado: 11 - Qual é o comando utilizado para executar o compilador da linguagem Java?\na. JavaC\nb. javac\nc. java-compiler\nd. java-run\ne. jc");
+				System.out.print("Resposta: b. javac");
 			break;
 			case "12":
-			
+				System.out.print("\nEnunciado: 12 - Qual é o comando utilizado para executar um programa Java? \na. javac\nb. JAVA\nc. java\nd. java-run\ne. jvm");
+				System.out.print("Resposta: b. javac");
 			break;
 			case "13":
-			
+				System.out.print("\nEnunciado: 13 - Qual é a extensão dos arquivos que armazenam código compilado de um programa Java?\na. .java\nb. .class\nc. .exe\nd. .prog\ne. .sh");
+				System.out.print("Resposta: b. .class");
 			break;
 			case "14":
-			
+				System.out.print("\nEnunciado: 14 - Ao compilar o arquivo Programa.java , quantos arquivos com a extensão .class serão gerados?\nclass Maria {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Maria\");\n\t}\n}\nclass Lobato {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Lobato\");\n\t}\n}\nclass Jonas {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(\"Jonas\");\n\t}\n}\na. Nenhum\nb. Um\nc. Dois\nd. Três\ne. Quatro");
+				System.out.print("Resposta: d. Três");
 			break;
 			case "15":
-			
+				System.out.print("\nEnunciado: 1 \na. \nb. \nc. \nd. \ne. ");
+				System.out.print("Resposta: ");
 			break;
 			case "16":
-			
+				System.out.print("\nEnunciado: 1 \na. \nb. \nc. \nd. \ne. ");
+				System.out.print("Resposta: ");
 			break;
 			case "17":
-			
+				System.out.print("\nEnunciado: 1 \na. \nb. \nc. \nd. \ne. ");
+				System.out.print("Resposta: ");
 			break;
 			case "18":
-			
+				System.out.print("\nEnunciado: 1 \na. \nb. \nc. \nd. \ne. ");
+				System.out.print("Resposta: ");
 			break;
 			case "19":
-			
+				System.out.print("\nEnunciado: 1 \na. \nb. \nc. \nd. \ne. ");
+				System.out.print("Resposta: ");
 			break;
 			default:
-			
-			break;
-		}
+				System.out.print("\nEnunciado: 20 - \na. \nb. \nc. \nd. \ne. ");
+				System.out.print("Resposta: ");		}
 	}
 }
 
